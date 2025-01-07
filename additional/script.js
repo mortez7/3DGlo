8bcade4430c88fd6160b402fa989e98528908c7a
Ivan Sokolenko <ivan.sokolenko.2017@gmail.com>
2025-01-07 17:56:53 +0200

    Добавлено автообновление часов

--- additional/script.js
+++ additional/script.js
@@ -1,34 +1,44 @@
-const now = new Date();
-let resultStr = "";
 const weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-const day = weekDays[now.getDay()];
-let hours = now.getHours().toString().padStart(2, "0");
-const minutes = now.getMinutes().toString().padStart(2, "0");
-const seconds = now.getSeconds().toString().padStart(2, "0");
-const ampm = hours >= 12 ? "PM" : "AM";
-const newYearDate = new Date(now.getFullYear() + 1, 0, 1).getTime();
-const dateNow = now.getTime();
-const timeDifference = newYearDate - dateNow;
-const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-let timesOfDay = "";
 
-switch (true) {
-  case hours >= 0 && hours < 5:
-    timesOfDay = "Доброй ночи";
-    break;
-  case hours >= 5 && hours < 12:
-    timesOfDay = "Доброе утро";
-    break;
-  case hours >= 12 && hours < 18:
-    timesOfDay = "Добрый день";
-    break;
-  case hours >= 18 && hours <= 23:
-    timesOfDay = "Добрый вечер";
-    break;
-}
+const getTimeData = () => {
+  const now = new Date();
+  const day = weekDays[now.getDay()];
+  let hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+  const newYearDate = new Date(now.getFullYear() + 1, 0, 1).getTime();
+  const dateNow = now.getTime();
+  const timeDifference = newYearDate - dateNow;
+  const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let timesOfDay = "";
+  switch (true) {
+    case hours >= 0 && hours < 5:
+      timesOfDay = "Доброй ночи";
+      break;
+    case hours >= 5 && hours < 12:
+      timesOfDay = "Доброе утро";
+      break;
+    case hours >= 12 && hours < 17:
+      timesOfDay = "Добрый день";
+      break;
+    case hours >= 17 && hours <= 23:
+      timesOfDay = "Добрый вечер";
+      break;
+  }
 
-hours = hours % 12 || 12;
-hours = String(hours).padStart(2, "0");
-resultStr = `${timesOfDay}<br>Сегодня: ${day}<br>Текущее время: ${hours}:${minutes}:${seconds} ${ampm}<br>До нового года осталось ${daysDifference} дней`;
+  return { timesOfDay, day, hours, minutes, seconds, ampm, daysDifference };
+};
 
-document.body.innerHTML = resultStr;
+const timeUpdate = () => {
+  const getTime = getTimeData();
+
+  let hours = getTime.hours % 12;
+  hours = hours ? hours : 12;
+  hours = String(hours).padStart(2, "0");
+  let resultStr = `${getTime.timesOfDay}<br>Сегодня: ${getTime.day}<br>Текущее время: ${hours}:${getTime.minutes}:${getTime.seconds} ${getTime.ampm}<br>До нового года осталось ${getTime.daysDifference} дней`;
+
+  document.body.innerHTML = resultStr;
+};
+
+setInterval(timeUpdate, 1000);
